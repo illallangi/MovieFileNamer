b90a8b8e0c619ab1226f0f17cb47906a9bca9ceb
Andrew Cole <andrew.cole@illallangi.com>
2013-06-27 21:43:03 +1000

    Added CheckIfHasFiles

---
+++ Illallangi.MovieFileNamer/Checks/CheckIfHasFiles.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using System.Linq;
+using Illallangi.MovieFileNamer.Model;
+
+namespace Illallangi.MovieFileNamer.Checks
+{
+    public sealed class CheckIfHasFiles : CheckBase
+    {
+        public override bool Passes(MovieDbResult movie, string directory, Result result)
+        {
+            if (!Directory.GetFiles(directory).Any())
+            {
+                result.AddError(Path.GetFileName(directory), @"""{0}"" has no files;", Path.GetFileName(directory));
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
